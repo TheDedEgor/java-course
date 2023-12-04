abbd6f025e1e8e7afab78cdc8056f2a43a2156d9
Egor <korepin@dna-technology.ru>
2023-12-04 10:29:14 +0300

    Correction mistakes

--- src/main/java/edu/hw7/Task3.java
+++ src/main/java/edu/hw7/Task3.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Task3 implements PersonDatabase {
     private final Map<Integer, Person> personMap = new HashMap<>();
@@ -13,43 +11,29 @@ public class Task3 implements PersonDatabase {
     private final Map<String, List<Integer>> addressIndex = new HashMap<>();
     private final Map<String, List<Integer>> phoneIndex = new HashMap<>();
 
-    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-
     @Override
     public synchronized void add(Person person) {
-        var writeLock = readWriteLock.writeLock();
-        writeLock.lock();
-        try  {
-            personMap.put(person.id(), person);
+        personMap.put(person.id(), person);
 
-            var id = person.id();
-            var name = person.name();
-            var address = person.address();
-            var phoneNumber = person.phoneNumber();
+        var id = person.id();
+        var name = person.name();
+        var address = person.address();
+        var phoneNumber = person.phoneNumber();
 
-            if (isNotEmpty(name) && isNotEmpty(address) && isNotEmpty(phoneNumber)) {
-                nameIndex.computeIfAbsent(name, (item) -> new ArrayList<>()).add(id);
-                addressIndex.computeIfAbsent(address, (item) -> new ArrayList<>()).add(id);
-                phoneIndex.computeIfAbsent(phoneNumber, (item) -> new ArrayList<>()).add(id);
-            }
-        } finally {
-            writeLock.unlock();
+        if (isNotEmpty(name) && isNotEmpty(address) && isNotEmpty(phoneNumber)) {
+            nameIndex.computeIfAbsent(name, (item) -> new ArrayList<>()).add(id);
+            addressIndex.computeIfAbsent(address, (item) -> new ArrayList<>()).add(id);
+            phoneIndex.computeIfAbsent(phoneNumber, (item) -> new ArrayList<>()).add(id);
         }
     }
 
     @Override
     public synchronized void delete(int id) {
-        var writeLock = readWriteLock.writeLock();
-        writeLock.lock();
-        try {
-            Person person = personMap.remove(id);
-            if (person != null) {
-                removeFromIndex(nameIndex, person.name(), id);
-                removeFromIndex(addressIndex, person.address(), id);
-                removeFromIndex(phoneIndex, person.phoneNumber(), id);
-            }
-        } finally {
-            writeLock.unlock();
+        Person person = personMap.remove(id);
+        if (person != null) {
+            removeFromIndex(nameIndex, person.name(), id);
+            removeFromIndex(addressIndex, person.address(), id);
+            removeFromIndex(phoneIndex, person.phoneNumber(), id);
         }
     }
 
@@ -79,24 +63,18 @@ public class Task3 implements PersonDatabase {
     }
 
     private List<Person> findByIndex(Map<String, List<Integer>> index, String key) {
-        var readLock = readWriteLock.readLock();
-        readLock.lock();
-        try {
-            List<Integer> ids = index.get(key);
-            if (ids != null) {
-                List<Person> result = new ArrayList<>();
-                for (int id : ids) {
-                    Person person = personMap.get(id);
-                    if (person != null) {
-                        result.add(person);
-                    }
+        List<Integer> ids = index.get(key);
+        if (ids != null) {
+            List<Person> result = new ArrayList<>();
+            for (int id : ids) {
+                Person person = personMap.get(id);
+                if (person != null) {
+                    result.add(person);
                 }
-                return result;
             }
-            return List.of();
-        } finally {
-            readLock.unlock();
+            return result;
         }
+        return List.of();
     }
 
     private static boolean isNotEmpty(String s) {
